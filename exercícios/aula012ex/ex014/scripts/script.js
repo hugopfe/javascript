af0c89d408a737b3cecbd2a7eb2ed4e82f7fe2c7
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-08-05 15:50:28 -0300

    Update script.js

--- exercícios/aula012ex/ex014/scripts/script.js
+++ exercícios/aula012ex/ex014/scripts/script.js
@@ -2,15 +2,19 @@ const body = window.document.body
 const divTime = document.querySelector('#div-time')
 const img = document.querySelector('#div-image > img')
 const divGreeting = document.querySelector('#div-greeting')
-var sysTime = 18//Number(new Date().getHours())
+var sysTime = Number(new Date().getHours())
 
 divTime.innerHTML = `Agora são ${sysTime} horas`
 
 if (sysTime >= 12 && sysTime < 18) {
+    // Tarde
+    
     img.src = 'images/afternoon.png'
     body.style.backgroundColor = 'var(--collor-afternoon)'
     divGreeting.innerHTML = 'Boa tarde! Leve Jesus para onde for no seu dia &#x1F604;'
 } else if (sysTime >= 18 || sysTime < 6) {
+    // Noite
+    
     img.src = 'images/night.png'
     body.style.backgroundColor = 'var(--collor-night)'
     divGreeting.innerHTML = 'Boa noite! Deite-se em paz pois é Deus quem guarda sua casa &#x1F6E1;&#xFE0F;'
