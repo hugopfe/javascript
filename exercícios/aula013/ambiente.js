dfc68309aa8274d29ba439a6aabced6fe64ccad9
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-08-07 14:55:58 -0300

    Create aula013

---
+++ exercícios/aula013/ambiente.js
@@ -0,0 +1,18 @@
+var count = 1
+
+while (count <= 5) {
+    console.log(count)
+
+    switch (count) {
+        case 3:
+            console.log('Shelyshy: Árvores!')
+            break
+    }
+    
+    count++
+}
+
+do {
+    console.log(count)
+    count--
+} while (count > 1)
