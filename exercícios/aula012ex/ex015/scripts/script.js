74156d1152fe90ca7dfe248e1f2e054a60c3d089
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-08-07 14:55:49 -0300

    minor changes

--- exercícios/aula012ex/ex015/scripts/script.js
+++ exercícios/aula012ex/ex015/scripts/script.js
@@ -4,7 +4,8 @@ function showResult() {
 
     // Get user data
     let birthYear = Number(document.querySelector('#div-form #id-year').value)
-    let genre = document.querySelector('#genre-m').checked == true?'Homem':'Mulher'
+    // let genre = document.querySelector('#genre-m').checked == true?'Homem':'Mulher'
+    let genre = document.getElementsByName('genre')
     let divResult = document.querySelector('#div-result')
     
     // Calculate data
@@ -16,20 +17,26 @@ function showResult() {
     if (birthYear == 0 || birthYear > sysYear) {
         alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        if (genre == 'Homem') {
+        if (genre[0].checked) {
+            divResult.innerHTML = `<p>Um ${genre[0].value} de ${age} anos.</p>`
+            document.body.style.backgroundColor = 'rgb(0, 77, 192)'
+
             if (age < 12) {
                 imageName = 'child-man.png'
-            } else if (age < 30) {
+            } else if (age < 21) {
                 imageName = 'young-man.png'
             } else if (age < 50) {
                 imageName = 'adult-man.png'
             } else {
                 imageName = 'old-man.png'
             }
-        } else if (genre == 'Mulher') {
+        } else if (genre[1].checked) {
+            divResult.innerHTML = `<p>Uma ${genre[1].value} de ${age} anos.</p>`
+            document.body.style.backgroundColor = 'rgb(255 106 197)'
+
             if (age < 12) {
                 imageName = 'child-woman.png'
-            } else if (age < 30) {
+            } else if (age < 21) {
                 imageName = 'young-woman.png'
             } else if (age < 50) {
                 imageName = 'adult-woman.png'
@@ -38,9 +45,8 @@ function showResult() {
             }
         }
 
-    divResult.innerHTML = `<p>${genre == 'Homem'?'Um':'Uma'} ${genre} de ${age} anos.</p>`
     let imageNode = document.createElement('img')
-    imageNode.src = 'images/' + imageName
+    imageNode.setAttribute('src', 'images/' + imageName)
     divResult.appendChild(imageNode)
     }
     
