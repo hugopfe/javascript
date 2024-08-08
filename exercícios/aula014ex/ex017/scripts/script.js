f668463182c62c575864f99b8bea068b586805d3
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-08-08 15:13:46 -0300

    Create aula014/ex017

---
+++ exercícios/aula014ex/ex017/scripts/script.js
@@ -0,0 +1,22 @@
+function generateMultiTable() {
+    // Getting HTML elements
+    const formFactor = document.getElementById('id-number')
+    const slctMultiTable = document.querySelector('#id-multi-table')
+
+    // Processing data
+    if (formFactor.value.length == 0) {
+        alert('Impossível gerar tabuada. \nCheque os dados e tente novamente.')
+    } else {
+        // Getting user form data
+        let factor = Number(formFactor.value)
+
+        slctMultiTable.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let optCalc = document.createElement('option')
+            let txtCalc = document.createTextNode(`${factor} x ${c} = ${factor * c}`)
+
+            optCalc.appendChild(txtCalc)
+            slctMultiTable.appendChild(optCalc)
+        }
+    }
+}
