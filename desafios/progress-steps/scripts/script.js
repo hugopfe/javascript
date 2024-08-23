e0b2be02c6dac73e911e8a49b43bb94978f8923a
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-08-23 09:27:57 -0300

    Finalized Progress-Steps scripts

--- desafios/progress-steps/scripts/script.js
+++ desafios/progress-steps/scripts/script.js
@@ -1,10 +1,34 @@
 const liSteps = document.getElementsByTagName('li')
+const prevButton = document.getElementById('prev-btn')
+const nextButton = document.getElementById('next-btn')
 
 function nextStep() {
+    let liActivatedSteps = document.getElementsByClassName('actual-step')
+
     for (let c in liSteps) {
-        if (liSteps[c].id != 'atual-step') {
-            liSteps[c].id = 'actual-step'
+        if (liSteps[c].classList[0] != 'actual-step') {
+            liSteps[c].classList.add('actual-step')
+            
+            if (liActivatedSteps.length > 1 && prevButton.classList.contains('inactive')) {
+                prevButton.classList.remove('inactive')
+            } else if (liActivatedSteps.length == 4) {
+                nextButton.classList.add('inactive')
+            }
             break
         }
+    }
+}
+
+function prevStep() {
+    let liActivatedSteps = document.getElementsByClassName('actual-step')
+
+    liActivatedSteps[liActivatedSteps.length-1].classList.remove('actual-step')
+
+    if (liActivatedSteps.length == 1) {
+        prevButton.classList.add('inactive')
+    }
+
+    if (liActivatedSteps.length < 4) {
+        nextButton.classList.remove('inactive')
     }
 }
