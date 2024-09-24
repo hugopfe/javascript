52e110f863d7ed1b3df3d688afab9e2a3a3d8d4c
Hugo Fernandes <75183558+hugopfe@users.noreply.github.com>
2024-09-24 09:10:17 -0300

    Change get verse script and styles

--- desafios/bible-verses/scripts/script.js
+++ desafios/bible-verses/scripts/script.js
@@ -1,27 +1,39 @@
 const divVerse = document.querySelector('div#verse')
 const button = document.querySelector('button')
-const arrVerse = ['efesios 2:8', 'filipenses 2:5-11', 'hebreus 4:12', 'apocalipse 19:11-16', 'proverbios 4:18', 'galatas 5:16', '2co 12:9', 'romanos 8:28', 'joão 6:68', 'salmos 42:1', 'salmos 16:11', 'isaias 43:1-2', 'isaias 49:15', 'isaias 55:6']
+const arrVerse = [
+    'efesios 2:8',
+    'filipenses 2:5-11', 
+    'hebreus 4:12', 
+    'apocalipse 19:11-16',
+    'proverbios 4:18',
+    'galatas 5:16',
+    '2co 12:9',
+    'romanos 8:28',
+    'joão 6:68',
+    'salmos 42:1',
+    'salmos 16:11',
+    'isaias 43:1-2',
+    'isaias 49:15',
+    'isaias 55:6'
+]
 var lastVerse = ''
 
 button.addEventListener ('click', () => {
-    requestBibleApi(getRandomVerse())
+    requestBibleApi(getVerse())
 })
 
-window.addEventListener('load', () => {requestBibleApi(getRandomVerse())})
+window.addEventListener('load', () => {requestBibleApi(getVerse())})
 
-function getRandomVerse() {
-    let v = lastVerse
-    while (v == lastVerse) {
-        v = arrVerse[Math.floor(Math.random()*arrVerse.length)]
-    }
-    lastVerse = v
-    return lastVerse
+function getVerse() {
+    let nextVerse = arrVerse[0]
+    arrVerse.push(arrVerse.shift())
+    return nextVerse
 }
 
 async function requestBibleApi (ref='john 3:16') {
     fetch(`https://bible-api.com/${ref}?translation=almeida`).then(res => {
         return res.json()
-    }).then(verse => setVerse(`<p>${verse.text}<br><b>${verse.reference}</b></p>`))
+    }).then(verse => setVerse(`<p>${verse.text}</p><p id="ref">${verse.reference}</p>`))
 }
 
 function setVerse (text) {
